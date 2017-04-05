1a678d907f2a593271bcc7e55c7d7e90071eb65f
Pri Bhula <pridotbhula@gmail.com>
2017-04-06 11:17:03 +1200

    Updated Feature class
    ye boi

--- src/Feature.java
+++ src/Feature.java
@@ -2,4 +2,40 @@
  * Created by Pri on 2/04/2017.
  */
 public class Feature {
+
+    int[] rows;
+    int[] cols;
+    boolean[] signs;
+
+    public Feature(int[] row, int[] col, boolean[] sign){
+        this.rows=row;
+        this.cols=col;
+        this.signs=sign;
+    }
+
+    public int[] getRows(){
+        return rows;
+    }
+
+    public void setRows (int[] r){
+        this.rows = r;
+    }
+
+    public int[] getCols(){
+        return cols;
+    }
+
+    public void setCols(int[] c){
+        this.cols = c;
+    }
+
+    public boolean[] getSigns(){
+        return signs;
+    }
+
+    public void setSigns(boolean[] s){
+        this.signs =    s;
+    }
+
+
 }
